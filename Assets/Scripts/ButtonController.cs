1ea2f8b20358eb17383d31fc2e6f5fd2c24626c7
kunmura <tvfrog99@gmail.com>
2014-08-15 17:30:55 +0900

    MyPage一旦コミット
    * 背景画像表示
    * ボタン表示

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+
+	public void onClickQuestGate(){
+		Debug.Log("QuestGate");
+	}
+
+	public void onClickShop(){
+		Debug.Log("Shop");
+	}
+
+	public void onClickGacha(){
+		Debug.Log("Gacha");
+	}
+
+	public void onClickTeam(){
+		Debug.Log("Team");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
