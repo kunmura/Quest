fad9c03c3205eeb4702d623562e3483936dcf759
kunmura <tvfrog99@gmail.com>
2014-09-05 19:25:26 +0900

    新冒険バトルレイアウト
    * 新レイアウト
    * 背景切り替え機能
    * おおかみの簡易アニメーション

---
+++ Assets/Scripts/BgController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgController : MonoBehaviour {
+
+	SpriteRenderer MainSpriteRenderer;
+	// publicで宣言し、inspectorで設定可能にする
+	public Sprite StandbySprite;
+	public Sprite HoldSprite;
+	public bool lineBool;
+	
+	void Start ()
+	{
+		// このobjectのSpriteRendererを取得
+		MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		lineBool = true;
+	}
+	
+	void ChangeStateToStandby()
+	{
+		MainSpriteRenderer.sprite = StandbySprite;
+	}
+	void ChangeStateToHold()
+	{
+		MainSpriteRenderer.sprite = HoldSprite;
+	}
+
+	public void OnClick(){
+		Debug.Log("BgChange");
+		if(lineBool==true){
+			ChangeStateToHold();
+			lineBool=false;
+		}else{
+			ChangeStateToStandby();
+			lineBool=true;
+		}
+	}
+}
---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine;
+
+public class UnitController : MonoBehaviour {
+
+	public void OnClick() {
+		Debug.Log("OnClick");
+		SkeletonAnimation ookami = GameObject.Find("Ookami0").GetComponent<SkeletonAnimation>();
+		if(ookami.AnimationName == "attack"){
+			ookami.AnimationName = "stand";
+		}else{
+			ookami.AnimationName = "attack";
+		}
+	}
+
+}
